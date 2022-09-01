532d77323ab066ad8cfbf817d75ae067acd68ae5
SwZap <schenad4@gmail.com>
2022-09-01 12:26:04 -0500

    Add files via upload

--- Plot_a_Slice.m
+++ Plot_a_Slice.m
@@ -5,16 +5,18 @@ numFiles = length(fullFileNames);
 % Loop over all files reading them in and plotting them.
 for k = 1 : numFiles
     fprintf('Now plotting file %s\n', fullFileNames{k});
-    filename = fullFileNames{k};
-    A = readmatrix(filename);
-    Y = A(:,1);
-    A(:,1) = [];
-    Z = int16(A);
-    Dimension = size(Z);
-    X = int16([1:Dimension(1,2)]);
+    filename = fullFileNames{k}; % The fullFileNames also contains directory!
+    A = readmatrix(filename); % Automatically skip the headers and directly read the matrix
+    Y = A(:,1); % Read the first column
+    A(:,1) = []; % Delete the first column
+    Z = int32(A); % The rest is the intensity, make sure there is NO OVERFLOW
+    Dimension = size(Z); 
+    X = int16([1:Dimension(1,2)]); % Make X axis
     contourf(X,Y,Z,50,'LineColor', 'none');
-    set(gcf,'position',[100,100,400,600])
-    saveas(gcf,append(erase(filename,".txt"),'.png')) ;
+    colorbar
+    %caxis([3e4 3.5e4]) % Select the specific scale
+    set(gcf,'position',[100,100,400,600]) % Choose the image dimension 400x600
+    saveas(gcf,append(erase(filename,".txt"),'.png')) ; % Replace the extension, DO NOT have ".txt" in the directory!
     %% Laplacian Filter
     % sigma = .8;
     % alpha = 0.3;
